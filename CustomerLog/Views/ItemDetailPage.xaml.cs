94782ffa0258136897bf59c2dedbdaa7abd62f1f
Andrew Chiziba <chizibaandrew@gmail.com>
2022-01-20 19:23:30 +0200

    Add project files.

---
+++ CustomerLog/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CustomerLog.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CustomerLog.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CustomerLog/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CustomerLog.Models;
+using CustomerLog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomerLog.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
